32b3ed6bc1a952089c672e64609dab13835599c5
Ashfaq Robin <ashfaq.robin36@gmail.com>
2021-12-25 20:39:15 +0600

    First commit for feedback app.

--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -6,7 +6,7 @@ const FeedbackData = [
     },
     {
         id: 2,
-        rating: 5,
+        rating: 7,
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
     },
     {
